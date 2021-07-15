f474590a595a75cd5ac478bf8132c2b55dc0b2c5
Dmitry Gorkun <4750533+dmitrygorkun@users.noreply.github.com>
2021-07-15 15:42:15 +0300

    Fixed translated filter definition

--- src/common/lang/translated-filter.dto.ts
+++ src/common/lang/translated-filter.dto.ts
@@ -1,9 +1,9 @@
 import {IsEnum, IsOptional} from 'class-validator';
-import {Constructable} from '../entity/entity';
+import {Constructable, EntityListFilter} from '../entity/entity';
 import {LANG} from './translated';
 import {TranslatedFilter} from './translated-filter';
 
-export function TranslatedFilterDto<T extends Constructable<TranslatedFilter>>(Base: T) {
+export function TranslatedFilterDto<T extends Constructable<EntityListFilter>>(Base: T) {
 
     class Mixin extends Base implements TranslatedFilter {
 
