67b9a59b1f73c8f5abd24074d5a2a8c8f4e72ab9
Dmitry Gorkun <4750533+dmitrygorkun@users.noreply.github.com>
2021-07-26 13:28:15 +0300

    Added image mime enum

--- src/common/image/image.definition.ts
+++ src/common/image/image.definition.ts
@@ -1,3 +1,12 @@
+export enum IMAGE_MIME {
+    JPEG = 'image/jpeg',
+    PNG = 'image/png',
+    WEBP = 'image/webp',
+    SVG = 'image/svg+xml',
+}
+
+export const RASTER_IMAGES: ReadonlyArray<IMAGE_MIME> = [IMAGE_MIME.PNG, IMAGE_MIME.JPEG, IMAGE_MIME.WEBP];
+
 export interface Image {
     alt: string;
     title: string;
